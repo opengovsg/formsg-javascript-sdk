06c90ea38c4b7eae293fdaa8530907322b230394
Yuan Ruo <liangyuanruo@gmail.com>
2020-03-02 13:51:02 +0800

    Version 0.1.1 - Readme, configuration bugfix (#1)
    * bugfix for destructuring error
    * Add readme
    * 0.1.1

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const webhooks = require('./src/webhooks.js')
  * @param {string} [options.mode] If set to 'staging' this will initialise
  * the SDK for the FormSG staging environment
  */
-module.exports = function ({ mode }) {
+module.exports = function ({
+  mode='production'
+} = {}) {
     return {
       webhooks: webhooks({ mode })
     }
