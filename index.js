274b7830aa172adea09e1d23c2268ed555ef30f5
Yuan Ruo <liangyuanruo@gmail.com>
2020-03-02 18:41:35 +0800

    Version 0.2.0 - Add signing functions, end-to-end testing (#2)

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const webhooks = require('./src/webhooks.js')
  * @param {Object} options
  * @param {string} [options.mode] If set to 'staging' this will initialise
  * the SDK for the FormSG staging environment
+ * @param {string} [options.webhookSecretKey] Optional base64 secret key for signing webhooks
  */
 module.exports = function ({
-  mode='production'
+  mode='production',
+  webhookSecretKey,
 } = {}) {
     return {
-      webhooks: webhooks({ mode })
+      webhooks: webhooks({ mode, webhookSecretKey })
     }
 }
