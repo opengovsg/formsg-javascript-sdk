67f4731c82f2dee1a2799136bf48224947a682f9
liangyuanruo <liangyuanruo@gmail.com>
2020-03-20 16:28:31 +0800

    Add encrypt/decrypt functionality which mirrors the existing code in FormSG today, with minor changes
    - Renamed functions to expose a decrypt/encrypt method
    - To be consistent with decrypt(), encrypt() now accepts an array of
    Response objects instead of a JSON string
    - Renamed privateKey to secretKey for consistency with nacl library

--- src/crypto.js
+++ src/crypto.js
@@ -1,7 +1,64 @@
 'use strict'
 
-const tweetnacl = require('tweetnacl')
-const { decodeBase64, encodeUTF8 } = require('tweetnacl-util')
+const nacl = require('tweetnacl')
+const {
+  encodeBase64,
+  decodeBase64,
+  encodeUTF8,
+  decodeUTF8
+} = require('tweetnacl-util')
+
+/**
+ * A field type available in FormSG as a string
+ * @typedef {FieldType}
+ * @type {string}
+ * @example
+ * 'section'
+ * 'radiobutton'
+ * 'dropdown'
+ * 'checkbox'
+ * 'nric'
+ * 'email'
+ * 'table'
+ * 'number'
+ * 'rating'
+ * 'yes_no'
+ * 'decimal'
+ * 'textfield' // Short Text
+ * 'textarea' // Long Text
+ * 'attachment'
+ * 'date'
+ * 'mobile'
+ */
+
+/**
+ * The complete Triforce, or one or more components of the Triforce.
+ * @typedef {Response}
+ * @property {string} _id - The field ID of the response
+ * @property {string} question - The form question
+ * @property {string} answer - The answer to the form
+ * @property {FieldType} fieldType - The field type
+ */
+
+/**
+ * Encrypts submission with a unique keypair for each submission
+ * @param {String} formPublicKey base64
+ * @param {Response[]} responses Array of Response objects
+ * @returns encrypted basestring containing the submission public key,
+ * nonce and encrypted data in base64
+ * @throws error if any of the encrypt methods fail
+ */
+function encrypt (formPublicKey, responses) {
+  const submissionKeypair = generate()
+  const nonce = nacl.randomBytes(24)
+  const encrypted = encodeBase64(nacl.box(
+    decodeUTF8(JSON.stringify(responses)),
+    nonce,
+    decodeBase64(formPublicKey),
+    decodeBase64(submissionPrivateKey)
+  ))
+  return `${submissionKeypair.publicKey};${encodeBase64(nonce)}:${encrypted}`
+}
 
 /**
  * Decrypts an encrypted submission.
@@ -13,7 +70,7 @@ function decrypt (formPrivateKey, encryptedSubmission) {
   try {
     const [ submissionPublicKey, nonceEncrypted ] = encryptedSubmission.split(';')
     const [ nonce, encrypted ] = nonceEncrypted.split(':').map(decodeBase64)
-    const decrypted = tweetnacl.box.open(
+    const decrypted = nacl.box.open(
       encrypted,
       nonce,
       decodeBase64(submissionPublicKey),
@@ -25,8 +82,46 @@ function decrypt (formPrivateKey, encryptedSubmission) {
   }
 }
 
+/**
+ * A cryptographic keypair
+ * @typedef {Keypair}
+ * @property {string} publicKey The base-64 encoded public key
+ * @property {string} privateKey The base-64 encoded privateKey key
+ */
+
+/**
+ * Generates a new keypair for encryption.
+ * @returns {Keypair}
+ */
+function generate () {
+  const kp = nacl.box.keyPair()
+  return {
+    publicKey: encodeBase64(kp.publicKey),
+    secretKey: encodeBase64(kp.secretKey),
+  }
+}
+
+/**
+ * Checks whether a public & private keypair are associated with each other
+ * @param {string} publicKey 
+ * @param {string} secretKey 
+ */
+function valid (publicKey, secretKey) {
+  const text = 'testtext'
+  try {
+    const encrypted = encryptSubmission(publicKey, text)
+    const decrypted = decryptSubmission(secretKey, encrypted)
+    return decrypted === text
+  } catch (err) {
+    return false
+  }
+}
+
 module.exports = function () {
   return {
+    encrypt,
     decrypt,
+    generate,
+    valid,
   }
 }
