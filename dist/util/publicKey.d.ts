54664ec8bd7db7877b87ac56afe84540ee703a7c
Kar Rui Lau <karrui.lau@gmail.com>
2020-04-17 13:39:06 +0800

    chore: remove /dist directory files

--- dist/util/publicKey.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Retrieves the appropriate public key.
- * Defaults to production.
- * @param mode The package mode to retrieve the public key for.
- */
-declare function getPublicKey(mode?: PackageMode): string;
-export { getPublicKey };
--- dist/util/stage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var STAGE = {
-    staging: 'staging',
-    production: 'production',
-    development: 'development',
-    test: 'test',
-};
-exports.default = STAGE;
