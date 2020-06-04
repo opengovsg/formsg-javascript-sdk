8f054ca1f4881a998aab16566f365fffe2eabfc0
Kar Rui Lau <karrui.lau@gmail.com>
2020-06-04 16:36:56 +0800

    test: extract out commonly used functions across tests

--- spec/webhooks.spec.ts
+++ spec/webhooks.spec.ts
@@ -9,32 +9,44 @@ describe('Webhooks', () => {
   const submissionId = 'someSubmissionId'
   const formId = 'someFormId'
 
-  const webhook = new Webhooks({
+  const webhooks = new Webhooks({
     publicKey: webhooksPublicKey,
     secretKey: signingSecretKey,
   })
 
-  it('should be signing the signature and generating the X-FormSG-Signature header with the correct format', () => {
-    const epoch = 1583136171649
-    const signature = webhook.generateSignature({
+  /**
+   * Helper method to generate a test signature.
+   */
+  const generateTestSignature = (epoch: number) => {
+    return webhooks.generateSignature({
       uri,
       submissionId,
       formId,
       epoch,
-    }) as string
+    })
+  }
 
+  /**
+   * Helper method to construct a test header.
+   */
+  const constructTestHeader = (epoch: number, signature: string) => {
+    return webhooks.constructHeader({
+      epoch,
+      submissionId,
+      formId,
+      signature,
+    })
+  }
+
+  it('should be signing the signature and generating the X-FormSG-Signature header with the correct format', () => {
+    const epoch = 1583136171649
+    const signature = generateTestSignature(epoch)
     expect(signature).toBe(
       'KMirkrGJLPqu+Na+gdZLUxl9ZDgf2PnNGPnSoG1FuTMRUTiQ6o0jB/GTj1XFjn2s9JtsL5GiCmYROpjJhDyxCw=='
     )
 
     // X-FormSG-Signature
-    const header = webhook.constructHeader({
-      epoch,
-      submissionId,
-      formId,
-      signature,
-    }) as string
-
+    const header = constructTestHeader(epoch, signature)
     expect(header).toBe(
       `t=1583136171649,s=someSubmissionId,f=someFormId,v1=KMirkrGJLPqu+Na+gdZLUxl9ZDgf2PnNGPnSoG1FuTMRUTiQ6o0jB/GTj1XFjn2s9JtsL5GiCmYROpjJhDyxCw==`
     )
@@ -42,37 +54,18 @@ describe('Webhooks', () => {
 
   it('should authenticate a signature that was recently generated', () => {
     const epoch = Date.now()
-    const signature = webhook.generateSignature({
-      uri,
-      submissionId,
-      formId,
-      epoch,
-    }) as string
-    const header = webhook.constructHeader({
-      epoch,
-      submissionId,
-      formId,
-      signature,
-    }) as string
+    const signature = generateTestSignature(epoch)
+    const header = constructTestHeader(epoch, signature)
 
-    webhook.authenticate(header, uri)
+    const authentiateResult = webhooks.authenticate(header, uri)
+    expect(authentiateResult).toBe(true)
   })
 
   it('should reject signatures generated more than 5 minutes ago', () => {
     const epoch = Date.now() - 5 * 60 * 1000 - 1
-    const signature = webhook.generateSignature({
-      uri,
-      submissionId,
-      formId,
-      epoch,
-    }) as string
-    const header = webhook.constructHeader({
-      epoch,
-      submissionId,
-      formId,
-      signature,
-    }) as string
+    const signature = generateTestSignature(epoch)
+    const header = constructTestHeader(epoch, signature)
 
-    expect(() => webhook.authenticate(header, uri)).toThrow()
+    expect(() => webhooks.authenticate(header, uri)).toThrow()
   })
 })
