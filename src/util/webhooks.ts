e2b9ab3a0e2953c24f6c54926639e0c48096ce3e
Yuanruo Liang <yuanruo@open.gov.sg>
2021-01-27 14:49:59 +0800

    fix(epoch): allow for possible clock drift

--- src/util/webhooks.ts
+++ src/util/webhooks.ts
@@ -34,14 +34,15 @@ const isSignatureHeaderValid = (
 
 /**
  * Helper function to verify that the epoch submitted is recent and valid.
- * Prevents against replay attacks.
- * @param epoch The number of milliseconds since Jan 1, 1979
+ * Prevents against replay attacks. Allows for negative time interval
+ * in case of clock drift between Form servers and recipient server.
+ * @param epoch The number of milliseconds since 1 Jan 1970 00:00:00 UTC.
  * @param expiry Duration of expiry in milliseconds. The default is 5 minutes.
  * @returns true if the epoch given has exceeded expiry duration calculated from current time.
  */
 const hasEpochExpired = (epoch: number, expiry: number = 300000) => {
-  const difference = Date.now() - epoch
-  return difference <= 0 || difference > expiry
+  const difference = Math.abs(Date.now() - epoch)
+  return difference > expiry
 }
 
 export { isSignatureHeaderValid, hasEpochExpired }
