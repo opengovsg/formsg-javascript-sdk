e132d0a7412e42957e005bee91e0ee47b633a2a0
Frank Chen <frankchn@gmail.com>
2021-01-31 04:51:37 +0000

    chore: Update JSDoc and print warning message if deprecated mode parameter is used to initialize FormSG SDK

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ import Crypto from './crypto'
 /**
  * Entrypoint into the FormSG SDK
  * @param {PackageInitParams} config Package initialization config parameters
+ * @param {string} [config.publicKey] This public key is used to authenticate or verify signed objects passed into this package.
+ * @param {string?} [config.webhookSecretKey] Optional base64 secret key for signing webhooks
+ *//**
+ * Deprecated entrypoint into the FormSG SDK
+ *
+ * @deprecated since January 2021
+ * @param {PackageInitParams} config Package initialization config parameters
  * @param {string?} [config.publicKey] Optional. If provided, this public key will be used instead to authenticate or verify signed objects passed into this package.
  * @param {string?} [config.mode] Optional. Initializes public key used for verifying and decrypting in this package. If `config.signingPublicKey` is given, this param will be ignored.
  * @param {string?} [config.webhookSecretKey] Optional base64 secret key for signing webhooks
@@ -19,6 +26,10 @@ export = function (config: PackageInitParams = {}) {
     publicKey || getSigningPublicKey(mode || 'production')
   const verificationPublicKey =
     publicKey || getVerificationPublicKey(mode || 'production')
+
+  if (typeof mode !== 'undefined') {
+    console.warn('Initializing the FormSG SDK is now deprecated. Please switch to the publicKey parameter instead.')
+  }
 
   return {
     webhooks: new Webhooks({
