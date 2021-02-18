196525fb7f3ebba8e17d0f018f783a13042e8480
Kar Rui Lau <karrui.lau@gmail.com>
2021-02-18 13:57:59 +0800

    test(init): add test cases for parameter instantiation

--- spec/init.spec.ts
+++ spec/init.spec.ts
@@ -1,20 +1,32 @@
 import formsg from '../src/index'
-import {
-  getVerificationPublicKey,
-  getSigningPublicKey,
-} from '../src/util/publicKey'
-import { VERIFICATION_KEYS } from '../src/resource/verification-keys'
 import { SIGNING_KEYS } from '../src/resource/signing-keys'
-import Webhooks from '../src/webhooks'
-import Crypto from '../src/crypto'
-import Verification from '../src/verification'
-
-const TEST_PUBLIC_KEY = SIGNING_KEYS.test.publicKey
+import { VERIFICATION_KEYS } from '../src/resource/verification-keys'
+import {
+  getSigningPublicKey,
+  getVerificationPublicKey,
+} from '../src/util/publicKey'
 
 describe('FormSG SDK', () => {
   describe('Initialisation', () => {
     it('should be able to initialise without arguments', () => {
-      expect(() => formsg()).not.toThrow()
+      const sdk = formsg()
+      // Should be autopopulated with production public keys.
+      expect(sdk.crypto.signingPublicKey).toEqual(
+        SIGNING_KEYS.production.publicKey
+      )
+      expect(sdk.verification.verificationPublicKey).toEqual(
+        VERIFICATION_KEYS.production.publicKey
+      )
+      expect(sdk.webhooks.publicKey).toEqual(SIGNING_KEYS.production.publicKey)
+    })
+
+    it('should correctly assign given webhook signing key', async () => {
+      const mockSecretKey = 'mock secret key'
+      const sdk = formsg({
+        webhookSecretKey: mockSecretKey,
+      })
+
+      expect(sdk.webhooks.secretKey).toEqual(mockSecretKey)
     })
 
     it('should be able to initialise with valid verification options', () => {
@@ -58,30 +70,19 @@ describe('FormSG SDK', () => {
         VERIFICATION_KEYS.production.publicKey
       )
     })
-  })
 
-  it('should get the correct signing key given a mode', () => {
-    expect(getSigningPublicKey('test')).toBe(SIGNING_KEYS.test.publicKey)
-    expect(getSigningPublicKey('staging')).toBe(SIGNING_KEYS.staging.publicKey)
-    expect(getSigningPublicKey('development')).toBe(
-      SIGNING_KEYS.development.publicKey
-    )
-    expect(getSigningPublicKey('production')).toBe(
-      SIGNING_KEYS.production.publicKey
-    )
-    expect(getSigningPublicKey()).toBe(SIGNING_KEYS.production.publicKey)
-  })
-
-  it('should be able to initialise with given publicKey init param', () => {
-    // Act
-    const userSpecifiedKey = TEST_PUBLIC_KEY
-    // Create SDK with a specified public key
-    const sdk = formsg({ publicKey: userSpecifiedKey })
-
-    // Assert
-    // All various keys used by subpackages should be the given public key.
-    expect(sdk.crypto.publicSigningKey).toEqual(userSpecifiedKey)
-    expect(sdk.verification.verificationPublicKey).toEqual(userSpecifiedKey)
-    expect(sdk.webhooks.publicKey).toEqual(userSpecifiedKey)
+    it('should get the correct signing key given a mode', () => {
+      expect(getSigningPublicKey('test')).toBe(SIGNING_KEYS.test.publicKey)
+      expect(getSigningPublicKey('staging')).toBe(
+        SIGNING_KEYS.staging.publicKey
+      )
+      expect(getSigningPublicKey('development')).toBe(
+        SIGNING_KEYS.development.publicKey
+      )
+      expect(getSigningPublicKey('production')).toBe(
+        SIGNING_KEYS.production.publicKey
+      )
+      expect(getSigningPublicKey()).toBe(SIGNING_KEYS.production.publicKey)
+    })
   })
 })
